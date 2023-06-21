5a4cf4b4eaef9cb773ff48115d4e8c2cba0064f4
nsdhl <dhlnischal10@gmail.com>
2023-06-21 18:43:05 +0545

    app file and quiz data updated

---
+++ src/Data/QuizData.js
@@ -0,0 +1,13 @@
+const QuizData  = [
+    {
+        question: 'What is the capital of France?',
+        answers: [ "New York", "London", "Paris", "Dublin"],
+        correct: "Paris"
+    },
+
+    {
+        question: 'Who is CEO of Tesla?',
+        answers: [ "Jeff Bezos", "Elon Musk", "Bill Gates", "Tony Stark"],
+        correct: "Elon Musk"
+    },
+]
