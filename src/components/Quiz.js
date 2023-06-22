f621ebbbf2b6ca37acdf1adde53b6174b8acf016
nsdhl <dhlnischal10@gmail.com>
2023-06-22 22:19:26 +0545

    changes made in component file and css

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import { QuizData } from "../Data/QuizData";
-import "../App.css";
+import "../App.css"
 
 function Quiz() {
+
+  const  [currentQuestion, setCurrentQuestion] = useState (0);
+  const [score, setScore] = useState(0);
+  const [clickedOption, setClickedOption] = useState(0);
+
+  const changeQuestion = () => {
+    
+    if (currentQuestion === QuizData.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+    else {
+
+    }
+
+    const updateScore = () => {
+      if(clickedOption===QuizData[currentQuestion].answer){
+        setScore(score+1)
+      }
+    }
+    updateScore();
+  };
   return (
     <div>
-      <p className="heading-txt">Quiz App</p>
+    <div className="heading-txt">Quiz App</div>
       <div className="container">
         <div className="question-container">
-          <span id="question-no">Question 1</span>
-          <span id="question-txt">{QuizData[0].question}</span>
+          <span id="question-no">{currentQuestion+1}</span>
+          <span id="question-txt">{QuizData[currentQuestion].question}</span>
         </div>
         <div className="option-container">
-          {QuizData[0].options.map((option, index) => {
-            return <button className="option-btn">{option}</button>;
+          {QuizData[currentQuestion].options.map((option, index) => {
+            return <button className="option-btn" onClick={ ()=> {
+              setClickedOption(index +1)
+            }}>{option}</button>;
           })}
         </div>
 
-        <input type="button" value="Next" id="next-button" />
+        <input type="button"  value="Next" id="next-button" onClick={changeQuestion} />
       </div>
     </div>
   );
